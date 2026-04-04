d266b6919d56934cd1536a19476ef4e8e8f0ebe9
Ryanu01 <ryangomes08090109@gmail.com>
2026-04-04 20:59:05 +0530

    docker image code & docker file

--- worker/container/index.js
+++ worker/container/index.js
@@ -1,5 +1,6 @@
 const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3")
-const fs = require("fs/promises")
+const fs = require("node:fs/promises")
+const syncfs = require("node:fs")
 const path = require("node:path")
 const ffmpeg = require("fluent-ffmpeg")
 require("dotenv/config")
@@ -12,11 +13,7 @@ const RESOLUTIONS = [
 ]
 
 const client = new S3Client({
-    region: process.env.BUCKET_REGION,
-    credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY
-    }
+    region: "us-east-1"
 })
 
 const BUCKET_NAME = process.env.BUCKET_NAME
@@ -28,38 +25,45 @@ async function init() {
     })
     const result = await client.send(command);
     
-    const originalFilePath = `some-path`
+    const originalFilePath = `original-video.mp4`
     await fs.writeFile(originalFilePath, result.Body)
 
     const originalVideoPath = path.resolve(originalFilePath)
 
     const promises = RESOLUTIONS.map(resolution => {
-        const output = `transcoded/video-${resolution.name}.mp4`
-
-        return new Promise((resolve) => {
-            ffmpeg(originalVideoPath)
-            .output(output)
-            .withVideoCodec("Libx264")
-            .audioCodec("aac")
-            .withSize(`${resolution.width}x${resolution.height}`)
-            .on('end', async () => {
-                const putCommand = new PutObjectCommand({
-                    Bucket: "",
-                    Key: output
+        const output = `video-${resolution.name}-${KEY.split("/")[1]}`
+        try {
+            return new Promise((resolve) => {
+                ffmpeg(originalVideoPath)
+                .output(output)
+                .withVideoCodec("libx264")
+                .audioCodec("aac")
+                .withSize(`${resolution.width}x${resolution.height}`)
+                .on('end', async () => {
+                    const putCommand = new PutObjectCommand({
+                        Bucket: "final-videos.ryan.gomes",
+                        Key: output,
+                        Body: syncfs.createReadStream(path.resolve(output)),
+                        ContentType: "video/mp4",
+                        ContentDisposition: "inline",
+                    })
+                    await client.send(putCommand)
+                    console.log(`Uploaded ${output}`);
+                    
+                    resolve()
                 })
-                await client.send(putCommand)
-                console.log(`Uploaded ${output}`);
-                
-                resolve()
-            })
-            .format("mp4")
-            .run()
-        })
+                .format("mp4")
+                .run()
+            })    
+        } catch (error) {
+            console.log(error);
+            
+        }
+        
     })    
 
     await Promise.all(promises)
 
-
-
 } 
 
+init()
